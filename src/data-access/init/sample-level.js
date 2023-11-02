2a28f8ad1662cb3b2de2c9930cd80d3042953bf6
matiasbian <matiasezequielbian@gmail.com>
2023-11-02 19:28:34 -0300

    initial level required steps updated

--- src/data-access/init/sample-level.js
+++ src/data-access/init/sample-level.js
@@ -1,4 +1,4 @@
 module.exports = {
-    addLevel: "INSERT INTO `levels` (`level`, `label`, `structure`, `perfect_steps`) VALUES ('1', 'LEVEL_1', '{\"elements\":[{\"positionX\":0,\"positionY\":0,\"type\":\"start\"},{\"positionX\":1,\"positionY\":0,\"type\":\"movement\"},{\"positionX\":2,\"positionY\":0,\"type\":\"movement\"},{\"positionX\":3,\"positionY\":0,\"type\":\"finish\"}]}', '7');",
+    addLevel: "INSERT INTO `levels` (`level`, `label`, `structure`, `perfect_steps`) VALUES ('1', 'LEVEL_1', '{\"elements\":[{\"positionX\":0,\"positionY\":0,\"type\":\"start\"},{\"positionX\":1,\"positionY\":0,\"type\":\"movement\"},{\"positionX\":2,\"positionY\":0,\"type\":\"movement\"},{\"positionX\":3,\"positionY\":0,\"type\":\"finish\"}]}', '3');",
     existsLevel: "SELECT COUNT(*) AS count FROM levels"
 }
